71f1f9f425d228ab8bfb9a74c71edd5732a3bdb8
aluis <andreluis9214@gmail.com>
2017-11-08 22:54:28 -0200

    feat: Add user model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-module.exports = function App(app) {
-    app.get('/api', (req, res) => {
-        res.send({
-            status: 'ok',
-        });
-    });
-};
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+global.app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/expressauth',
+    {
+        useMongoClient: true,
+    },
+);
+
+require('./routes/index.js');
+
+app.listen(3000, () => {
+    process.stdout.write(`> Ready on http://localhost:${port}\n`);
+});
---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    phone: [
+        {
+            number: Number,
+            prefix: Number,
+        },
+    ],
+});
+
+module.exports = mongoose.model('User', schema);
