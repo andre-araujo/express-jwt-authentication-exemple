8447c92ba81c7b30e542081047fb71850583f130
aluis <andreluis9214@gmail.com>
2017-11-09 23:47:48 -0200

    feat: Add singup controller

--- src/controllers/singup.controller.js
+++ src/controllers/singup.controller.js
@@ -1,5 +1,39 @@
+const Account = require('../models/Account');
+
 function singup(req, res) {
-    res.send({ status: 'ok' });
+    const {
+        name,
+        email,
+        password,
+        phone,
+    } = req.body;
+
+    if (!name || !email || !password || !phone || !Array.isArray(phone)) {
+        res.send({ status: 'Missing information' });
+    }
+
+    const account = {
+        name,
+        email,
+        password,
+        singup_date: new Date(),
+        phone: phone.map(tel => ({
+            number: tel.number,
+            prefix: tel.prefix,
+        })),
+    };
+
+    Account.update(
+        { email },
+        account,
+        { upsert: true },
+        (err) => {
+            if (err) {
+                res.status(500).send({ status: err });
+            }
+            res.send({ status: 'ok' });
+        },
+    );
 }
 
 module.exports = singup;
