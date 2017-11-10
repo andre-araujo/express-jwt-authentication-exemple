f8584e50dc8352ad844b18e96bc6b5b9bf2b2c57
aluis <andreluis9214@gmail.com>
2017-11-09 23:48:04 -0200

    feat: Add token controller

---
+++ src/controllers/token.controller.js
@@ -0,0 +1,38 @@
+const jwt = require('jwt-simple');
+const { secret } = require('../constants');
+
+const Account = require('../models/Account');
+
+function tokenController(req, res) {
+    const {
+        email,
+        password,
+    } = req.body;
+
+    if (!email && !password) {
+        res.status(403).send({
+            status: 'Unauthorized',
+        });
+    }
+
+    Account.findOne({
+        email,
+        password,
+    }, (err, account) => {
+        if (err) {
+            res.status(500).send({ status: err });
+        }
+        if (account) {
+            const payload = { id: account._id };
+            const token = jwt.encode(payload, secret);
+
+            res.json({ token });
+        } else {
+            res.status(403).send({
+                status: 'Unauthorized',
+            });
+        }
+    });
+}
+
+module.exports = tokenController;
